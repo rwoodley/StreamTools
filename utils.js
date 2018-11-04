730f2da49abd18bf4be8f992ce2b812c42087002
Robert Woodley <blwoodley@yahoo.com>
2018-11-04 14:31:18 -0600

    many spinning balls

--- utils.js
+++ utils.js
@@ -150,3 +150,15 @@ function loadSkyBox(scene) {
         
         scene.add(aSkybox);
 }
+function logMediaDevices() {
+    navigator.mediaDevices.enumerateDevices()
+    .then(function (devices) {
+    devices.forEach(function (device) {
+        console.log(device.kind + ": |" + device.label +
+        "| id = " + device.deviceId);
+    });
+    })
+    .catch(function (err) {
+    console.log(err.name + ": " + err.message);
+    });
+}
